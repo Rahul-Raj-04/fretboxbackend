641c8eee232ca9b0f44ad40615f503993edf61dc
rahulraj <153027624+Rahul-Raj-04@users.noreply.github.com>
2025-03-05 13:16:44 +0530

    adfsdf

--- src/Middleware/auth.middleware.js
+++ src/Middleware/auth.middleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../Modules/User/User.model.js"
 export const protectRoute = async (req, res, next) => {
   try {
-
+    console.log("Cookies received in request:", req.cookies);
     const token = req.cookies.jwt;
 
     if (!token) {
