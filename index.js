8278e7da913476f552d5efec449c928b80da77bf
rahulraj <153027624+Rahul-Raj-04@users.noreply.github.com>
2025-03-05 12:55:38 +0530

    ddddddd

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ limit: "2500mb", extended: true })); // Increase li
 
 app.use(
       cors({
-            origin: process.env.CORS,
+            origin: "https://fretbox.brandbell.in",
             credentials: true,
       })
 );
