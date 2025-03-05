2bdca0072bdf72c6b87a75c90ace3c252b7f9374
rahulraj <153027624+Rahul-Raj-04@users.noreply.github.com>
2025-03-05 11:25:32 +0530

    ddad

--- src/Modules/Groups/Groupechat.routes.js
+++ src/Modules/Groups/Groupechat.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import multer from "multer";
-import { protectRoute } from "../../middleware/auth.middleware.js";
+import { protectRoute } from "../../Middleware/auth.middleware.js";
 import { createGroupChat, getGroupChats } from "./Groupchat.controller.js";
 
 
