3e347c18ab2b58281cfb95ab317aa246db03d952
rahulraj <153027624+Rahul-Raj-04@users.noreply.github.com>
2025-03-05 20:32:38 +0530

    dddsfsdfdsf

--- src/Modules/User/User.routes.js
+++ src/Modules/User/User.routes.js
@@ -12,7 +12,7 @@ import {
   updateUser,
 } from "./User.controler.js";
 
-import { protectRoute } from "../../middleware/auth.middleware.js";
+import { protectRoute } from "../../Middleware/auth.middleware.js";
 
 const router = express.Router();
 
